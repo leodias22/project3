ecfa98bd3c649d6c323d1be1327143d3f413beef
leodias22 <leodias22@gmail.com>
2020-07-20 16:14:00 -0300

    first commit on send email branch

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Sends email after filling the form
+  document.querySelector('#compose-form').addEventListener('submit', () => send_email());
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +33,12 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(){
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  console.log(recipients, subject, body);
+  
 }
